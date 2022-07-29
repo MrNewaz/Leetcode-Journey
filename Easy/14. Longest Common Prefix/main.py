cc823d82229eeb8bdc29629b7e97372a6964ab19
Saif Rahman <saif.rahman@udplatforms.com>
2022-07-29 17:31:53 +0600

    Day 3

--- Easy/14. Longest Common Prefix/main.py
+++ Easy/14. Longest Common Prefix/main.py
@@ -5,10 +5,14 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         res = ''
 
-        for i in range(len(str[0])):
+        for i in range(len(strs[0])):
             for s in strs:
                 if i >= len(s) or s[i] != strs[0][i]:
                     return res
             res += strs[0][i]
 
         return res
+
+
+x = Solution()
+print(x.longestCommonPrefix(["flower", "flow", "flight"]))
