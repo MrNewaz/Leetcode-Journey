6b7e43e2d27e2da9a3b5e647d4d6de6d66c0d8a3
Saif Rahman <saif.rahman@udplatforms.com>
2022-08-24 15:10:41 +0600

    Day 16

---
+++ Easy/326. Power of Three/main.py
@@ -0,0 +1,8 @@
+import math
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if not n or n < 0:
+            return False
+        return (math.log10(n) / math.log10(3)) % 1 == 0
