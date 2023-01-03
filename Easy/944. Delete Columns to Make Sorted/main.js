e4a4b7558811924a5558cc483b1b2d2034f7b3e7
Saif Rahman <saif.rahman@udplatforms.com>
2023-01-03 18:34:06 +0600

    init

--- Easy/944. Delete Columns to Make Sorted/main.js
+++ Easy/944. Delete Columns to Make Sorted/main.js
@@ -4,15 +4,25 @@
  */
 
 var minDeletionSize = function (strs) {
+  // Initialize a counter for the number of deletions needed
   let deletions = 0
+
+  // Iterate through the characters of the first string in the array
   for (let i = 0; i < strs[0].length; i++) {
+    // Iterate through the rest of the strings in the array
     for (let j = 1; j < strs.length; j++) {
+      // If the character at position i of the current string is lexicographically
+      // less than the character at position i of the previous string, a deletion
+      // is needed at this position
       if (strs[j][i] < strs[j - 1][i]) {
+        // Increment the deletion counter
         deletions++
+        // Break out of the inner loop, as any further comparisons are unnecessary
         break
       }
     }
   }
 
+  // Return the number of deletions needed
   return deletions
 }
