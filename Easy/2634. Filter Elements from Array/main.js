ebf328f77448538d7467d846de7325917759f05a
Saif Rahman <saif.rahman@udplatforms.com>
2023-06-25 00:30:40 +0600

    2634. Filter Elements from Array

--- Easy/2634. Filter Elements from Array/main.js
+++ Easy/2634. Filter Elements from Array/main.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function (arr, fn) {
-  return arr.filter((item, i) => fn(item, i, arr))
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
 }
