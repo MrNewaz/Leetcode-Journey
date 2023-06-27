8a148732cc2e19e17d487e1492b58473e8cc2a61
Saif Rahman <saif.rahman@udplatforms.com>
2023-06-28 02:57:36 +0600

    2723. Add Two Promises

--- Easy/2723. Add Two Promises/main.js
+++ Easy/2723. Add Two Promises/main.js
@@ -4,10 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {
-  const num1 = await promise1
-  const num2 = await promise2
-
-  return num1 + num2
+  return (await promise1) + (await promise2)
 }
 
 /**
