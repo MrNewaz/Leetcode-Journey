f46c1b58a0d51eb3cb5d8c5b2e5e360add11e108
Saif Rahman <saif.rahman@udplatforms.com>
2023-07-03 01:31:10 +0600

    128. Longest Consecutive Sequence

--- Medium/128. Longest Consecutive Sequence/main.js
+++ Medium/128. Longest Consecutive Sequence/main.js
@@ -3,26 +3,34 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-  if (nums == null || nums.length === 0) {
-    return 0
-  }
+  // edge case
+  if (nums == null || nums.length === 0) return 0
 
+  // declaring max and set
   let max = 0
   let set = new Set(nums)
 
+  // looping through set
   for (let num of set) {
+    // if set has the previous number then skip
     if (set.has(num - 1)) {
       continue
     }
+
+    // declaring currMax and currNum
     let currMax = 1
     let currNum = num
+
+    // looping until set no longer has the next number in the sequence
     while (set.has(currNum + 1)) {
       currNum++
       currMax++
     }
 
+    // setting max to the maximum of max and currMax
     max = Math.max(max, currMax)
   }
 
+  // returning max
   return max
 }
