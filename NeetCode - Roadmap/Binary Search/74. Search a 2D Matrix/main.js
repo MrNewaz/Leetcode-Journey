c7058e55da2cfd5b2153e98590b6801b4144c5c5
Saif Rahman <saif.rahman@udplatforms.com>
2023-07-23 23:49:24 +0600

    74. Search a 2D Matrix
    Co-authored-by: MrNewaz <mr.saifnewaz@gmail.com>

--- NeetCode - Roadmap/Binary Search/74. Search a 2D Matrix/main.js
+++ NeetCode - Roadmap/Binary Search/74. Search a 2D Matrix/main.js
@@ -4,27 +4,37 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
+  // Declare bottom and top pointers for searching the rows
   let top = 0,
     bottom = matrix.length - 1
 
   while (top <= bottom) {
+    // Declare mid pointer for searching the rows
     let mid = Math.floor((top + bottom) / 2)
 
+    // If target is greater than the last element of the row, then search the bottom half
     if (target > matrix[mid][matrix[mid].length - 1]) {
+      // Update top pointer to mid + 1 to search the bottom half
       top = mid + 1
     } else if (target < matrix[mid][0]) {
+      // Update bottom pointer to mid - 1 to search the top half
       bottom = mid - 1
     } else {
+      // Break out of the loop if target is within the range of the row
       break
     }
   }
 
+  // If target is not within the range of the matrix, return false
   if (!(top <= bottom)) return false
 
+  // Declare row pointer for searching the columns of the row
   let row = Math.floor((top + bottom) / 2)
+  // Declare left and right pointers for searching the columns of the row
   let left = 0,
     right = matrix[row].length - 1
 
+  // Binary search the last remaining row
   while (left <= right) {
     let mid = Math.floor((left + right) / 2)
     if (target > matrix[row][mid]) {
@@ -36,5 +46,9 @@ var searchMatrix = function (matrix, target) {
     }
   }
 
+  // Return false if target is not found
   return false
 }
+
+// Time Complexity: O(log(m) + log(n))
+// Space Complexity: O(1)
